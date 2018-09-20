40af1a60858d2635ccc64825b5c7a896861f0597
Caroline Berg <carolineeberg@gmail.com>
2018-09-20 21:02:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,15 @@
 class Triangle
   attr_accessor :length_one, :length_two, :length_three
   attr_reader :equilateral
+  @triangle = []
 
   def initialize(length_one, length_two, length_three)
     @length_one = length_one
     @length_two = length_two
     @length_three = length_three
+    @triangle << length_one 
+    @triangle << length_two 
+    @triangle << length_three
   end
 
   def equilateral=(equilateral)
