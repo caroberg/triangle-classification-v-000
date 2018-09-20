ddf325b3074a4f684076fa3d175feb9099222c65
Caroline Berg <carolineeberg@gmail.com>
2018-09-20 22:20:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
         raise TriangleError
           puts error.message
         end
-    elsif s1 == s2 && s2 == s3
+    elsif @s1 == @s2 && @s2 == @s3
       self.kind = :equilateral
     elsif s1 == s2 || s2 == s3 || s1 == s3
       self.kind = :isosceles
