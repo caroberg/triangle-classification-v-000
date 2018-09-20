188fd3641a566f739af8cdba29532d11c0b7c718
Caroline Berg <carolineeberg@gmail.com>
2018-09-20 22:19:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,5 @@
 class Triangle
-  attr_accessor :s1, :s2, :s3, :kind 
+  attr_accessor :s1, :s2, :s3, :kind
 
   def initialize(s1, s2, s3)
     @s1 = s1
@@ -7,24 +7,24 @@ class Triangle
     @s3 = s3
   end
 
-  def kind 
+  def kind
     if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s1 + s3) <= s2 || (s2 +s3) <= s1
-      begin 
-        raise TriangleError 
+      begin
+        raise TriangleError
           puts error.message
         end
-    elsif s1 == s2 && s2 == s3 
+    elsif s1 == s2 && s2 == s3
       self.kind = :equilateral
-    elsif s1 == s2 || s2 == s3 || s1 == s3 
-      self.kind = :isosceles 
-    else 
-      self.kind = :scalene 
-    end 
+    elsif s1 == s2 || s2 == s3 || s1 == s3
+      self.kind = :isosceles
+    else
+      self.kind = :scalene
+    end
   end
 end
 
-class TriangleError < StandardError 
-  def message 
+class TriangleError < StandardError
+  def message
     puts "I'M NOT A TRIANGLE, DUDE."
   end
 end
